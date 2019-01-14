44d27cb2376cf76be0735c63b414e56e66af1386
mikx <mm@izba.com>
2019-01-14 19:37:54 +0000

    simplify config

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/src/index.html`);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log('app listening on', port);
